8d29d7ec5d3799540c0225059bf31ade75dfa10e
aniurdhrajesh <anirudhrajesh04@gmail.com>
2025-02-11 18:37:46 +0530

    ACP

--- ACP.py
+++ ACP.py
@@ -1,15 +1,14 @@
 class Robot:
-    def __init__(self, name, model, purpose):
-        self.name = name
-        self.model = model
-        self.purpose = purpose
-    
-    def introduce(self):
-        print(f"Hello, I am {self.name}, a {self.model} model robot.")
-        print(f"My purpose is to {self.purpose}.")
-
-robot1 = Robot("RoboHelper", "XJ-300", "assist humans in daily tasks")
-robot2 = Robot("AutoServe", "YZ-200", "serve food and beverages")
-
-robot1.introduce()
-robot2.introduce()
+    def __init__(self,name,version,function):
+           self.name = name
+           self.version = version
+           self.function = function
+    def intro(self):
+          intro=(f"Hello,I am {self.name}, \n"
+                 f"I am version{self.version},\n"
+                 f"My Funtion is to{self.function}.")
+          return intro
+robot1=Robot("GaurdianBot","1.0"," Provide Security monitoring")
+robot2=Robot("AssistBot","2.0"," Help with household Chores")
+print(robot1.intro())
+print(robot2.intro())
